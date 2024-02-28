2a74603b41786be94bc6abf2a22b6878becbbad7
Christopher Morgan <60473703+chrislouis86@users.noreply.github.com>
2024-02-28 21:00:57 +0000

    Add files via upload

---
+++ Face dectection (mediapipe).py
@@ -0,0 +1,39 @@
+import mediapipe as mp
+import cv2
+
+mp_drawing=mp.solutions.drawing_utils
+mp_face_mesh=mp.solutions.face_mesh
+
+drawing_spec=mp_drawing.DrawingSpec( thickness=1,
+    circle_radius=1)
+
+video=cv2.VideoCapture(1)
+
+with  mp_face_mesh(min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as face_mesh:
+
+
+
+
+    while True:
+            ret.image=video.read()
+            image=cv2,cvtColor(image, cv2.COLOR_BRG2RGB)
+            image.flags.writeable=False
+            results=face_mesh.process(image)
+            #print(results)
+            if results.multi_face_landmarks:
+                for face_landmarks in results.multi_face_landmarks:
+                    mp_drawing.draw_landmarks( image=image, 
+                                                landmark_list=face_landmarks,
+                                                connections=mp.face_mesh.FACE_CONNECTIONS, 
+                                                landmark_drawing_spec=drawing+spec, 
+                                                connection_drawing_spec=drawing_spec)
+            cv2.imshow("Face Mesh", image)
+            
+            k=cv2.waitkey(1)
+            if k==ord('q'):
+                break
+
+    video.release()
+    cv2.destroyAllWindows()
+
